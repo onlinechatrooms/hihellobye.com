ee0194add7631a64028beb0e573b1234f46cbe23
tsts <tsts@gmail.com>
2020-09-17 00:04:58 -0500

    kkk

---
+++ hihellobye/chat.py
@@ -0,0 +1,4 @@
+def chat():
+	print("Hi")
+	print("Hello")
+	print("Bye")
---
+++ setup.py
@@ -0,0 +1,56 @@
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+
+if __name__ == '__main__':
+    setup(
+        name="hihellobye",
+
+        version='1.0.3',
+
+        description='Pakistani chat rooms for FREE',
+        long_description_content_type='text/markdown',
+        long_description=long_description,
+
+
+        author='hihellobye',
+        author_email='chat_room@hihellobye.com',
+
+        license='BSD2',
+        url = 'https://github.com/onlinechatrooms/hihellobye.com',
+
+        classifiers=[
+
+            'Development Status :: 5 - Production/Stable',
+
+            'Intended Audience :: Social network chat rooms',
+            'Intended Audience :: Entertainment',
+
+            'License :: OSI Approved :: BSD License',
+
+            'Natural Language :: English',
+
+            'Programming Language :: Python :: 3',
+
+            'Topic :: Pakistani Chat Rooms :: Chat Rooms',
+            'Topic :: Social Networks'
+        ],
+
+        keywords='chat room, online chat, chat rooms',
+
+
+
+        entry_points={
+            'console_scripts': [
+                'main=main:main'
+            ]
+        },
+
+    )
+
